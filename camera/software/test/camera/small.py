64fa538d999d5a4dcd91fc66b464dcf2fc244c1a
Jacob Cunningham <jcun4128@gmail.com>
2023-12-11 02:52:13 -0600

    test camera

--- camera/software/test/camera/small.py
+++ camera/software/test/camera/small.py
@@ -3,11 +3,13 @@ import time
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
+config = picam2.create_still_configuration(main={"size": (128, 128)})
+picam2.configure(config)
 
-preview_config = picam2.create_preview_configuration(main={"size": (128, 128)})
-picam2.configure(preview_config)
+# preview_config = picam2.create_preview_configuration(main={"size": (128, 128)})
+# picam2.configure(preview_config)
 
-picam2.start_preview(Preview.QTGL)
+# picam2.start_preview(Preview.QTGL)
 
 picam2.start()
 time.sleep(2)
