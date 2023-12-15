e30cefd19a872512027a85759745f7b58d4ffa97
Jacob Cunningham <jcun4128@gmail.com>
2023-12-15 02:20:46 -0600

    add simple photo switch test

---
+++ camera/software/test/oled/rapid-images.py
@@ -0,0 +1,50 @@
+# -*- coding:UTF-8 -*-
+
+import io
+import time
+from picamera2 import Picamera2
+
+#--------------Driver Library-----------------#
+import RPi.GPIO as GPIO
+import OLED_Driver as OLED
+
+#--------------Image Library---------------#
+from PIL  import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from PIL import ImageColor
+
+#----------------------MAIN-------------------------#
+
+def Display_Picture(File_Name):
+  image = Image.open(File_Name)
+  OLED.Display_Image(image)
+
+try:
+  def main():
+    #-------------OLED Init------------#
+    OLED.Device_Init()
+
+    img_id = 0
+
+    images = ["picture1.jpg", "picture2.jpg", "picture3.jpg"]
+
+    while True:
+      Display_Picture(images[img_id])
+
+      if (images < 2):
+        img_id += 1
+      else:
+          img_id = 0
+
+
+    if __name__ == '__main__':
+        main()
+
+# except Exception as e:
+except:
+    # print(e)
+    print("\r\nEnd")
+    OLED.Clear_Screen()
+    GPIO.cleanup()
+
