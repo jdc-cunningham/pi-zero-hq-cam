1862407cd9390c059834230751fa6e177f8da441
Jacob Cunningham <jcun4128@gmail.com>
2024-01-02 17:54:44 -0600

    reduce font size

--- camera/software/display/display.py
+++ camera/software/display/display.py
@@ -23,7 +23,6 @@ folder_sprite_path = base_path + "/menu/menu-sprites/folder_21_18.jpg"
 gear_sprite_path = base_path + "/menu/menu-sprites/gear_23_20.jpg"
 
 small_font = ImageFont.truetype(base_path + "/display/alt-font.ttc", 13)
-medium_font = ImageFont.truetype(base_path + "/display/alt-font.ttc", 14)
 large_font = ImageFont.truetype(base_path + "/display/alt-font.ttc", 16)
 
 class Display:
@@ -224,7 +223,7 @@ class Display:
     image = Image.new("RGB", (128, 128), "BLACK")
     draw = ImageDraw.Draw(image)
 
-    draw.text((22, 48), "Battery Charged?", fill = "WHITE", font = medium_font)
+    draw.text((22, 48), "Battery Charged?", fill = "WHITE", font = small_font)
     draw.text((22, 72), "Yes", fill = "CYAN" if is_charged else "WHITE", font = small_font)
     draw.text((60, 72), "No", fill = "WHITE" if is_charged else "CYAN", font = small_font) # default option
 
